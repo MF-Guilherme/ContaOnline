38eb212c44283a815a5472f2929b61b3d9d28e1a
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2023-04-26 21:18:20 -0300

    Projeto criado com alguns modelos e repositórios

---
+++ ContaOnline.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using ContaOnline.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaOnline.Domain.Interfaces
+{
+    public interface IRepository<T>
+    {
+        void Incluir(T contaCategoria);
+
+        void Alterar(T contaCategoria);
+
+        void Excluir(string id);
+
+        T ObterPorId(string id);
+
+        IEnumerable<T> ObterTodos();
+    }
+}
---
+++ ContaOnline.Domain/Models/ContaCategoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaOnline.Domain.Models
+{
+    public class ContaCategoria
+    {
+        public string Id { get; set; } // colocar o ID como string é mais interessante
+
+        public string Nome { get; set; }
+    }
+}
